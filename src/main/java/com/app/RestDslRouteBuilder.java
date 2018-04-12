63ccf88ec830bb46022e1b19659c4caa4a3c8c04
Ozymadias <saidamyzo@gmail.com>
2018-04-12 11:38:49 +0200

    Change Bean to two Processes

--- src/main/java/com/app/RestDslRouteBuilder.java
+++ src/main/java/com/app/RestDslRouteBuilder.java
@@ -1,9 +1,13 @@
 package com.app;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class RestDslRouteBuilder extends RouteBuilder {
@@ -17,7 +21,7 @@ public class RestDslRouteBuilder extends RouteBuilder {
          * included as a Maven dependency first.
          */
         restConfiguration()
-                .component("restlet")
+                .component("restlet")//todo: kto to robi pod spodem
                 .host("localhost").port("8085")
                 .bindingMode(RestBindingMode.json);
 
@@ -28,7 +32,21 @@ public class RestDslRouteBuilder extends RouteBuilder {
 //                .get()
 //                    .to("bean:helloBean");
 
-        from("rest:get:hello?name={name}").bean(Output.class, "create(${header.name})").marshal().json(JsonLibrary.Jackson);
+        from("rest:get:hello?name={name}").process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                Output output = new Output();
+                output.setMessage("Hello " + exchange.getIn().getHeaders().get("name"));
+                exchange.getIn().setBody(output);
+            }
+        }).process(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                Output body = exchange.getIn().getBody(Output.class);
+                body.setTime(LocalDateTime.now());
+                exchange.getOut().setBody(body);
+            }
+        })/*.bean(Output.class, "create(${header.name})")*/.marshal().json(JsonLibrary.Jackson);
 //                .transform().simple("Hello ${header.name}");
     }
 }
